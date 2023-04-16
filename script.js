beb4866628ec940d4c2cf20519e0d5ae1e6ea296
unknown0verlord <unknown0verlordxbox@gmail.com>
2023-04-16 18:55:50 -0400

    Added Farms, JS Comments, and quashed bugs

--- script.js
+++ script.js
@@ -8,29 +8,50 @@ let spudSpitterCost = 10;
 let farmer = 0;
 let farmerCost = 50;
 
+let farm = 0;
+let farmCost = 300;
+
+// "Make a Potato!" button  calls this
 function clickFirstButton() {
     increasePotatoes();
     document.getElementById("varCount").innerHTML = "Potatoes: " + potatoes;
     checkUpgrades();
 }
 
+// PpS implementation
 setInterval(() => {activatePpS();}, 1000);
 
+// Most Important Function
+// Takes number input from button press and directs to correct if-statement
+/////////////////////////////////////////////////////////////////////////////////
 
 function checkUpgrades(number) {
     if (potatoes >= spudSpitterCost && number == 1) {
         increaseSpudCount();
     } else if (potatoes >= spudSpitterCost) {
         document.getElementById("spudSpitter").style.backgroundColor = '#33cc33';
+    } else if (potatoes < spudSpitterCost) {
+        document.getElementById("spudSpitter").style.backgroundColor = '#006600';
     }
 
-    if (potatoes >= spudSpitterCost && number == 2) {
+    if (potatoes >= farmerCost && number == 2) {
         increaseFarmerCount();
     } else if (potatoes >= farmerCost) {
         document.getElementById("potatoFarmer").style.backgroundColor = '#33cc33';
+    } else if (potatoes < farmerCost) {
+        document.getElementById("potatoFarmer").style.backgroundColor = '#006600';
+    }
+
+    if (potatoes >= farmCost && number == 3) {
+        increaseFarmCount();
+    } else if (potatoes >= farmCost) {
+        document.getElementById("potatoFarm").style.backgroundColor = '#33cc33';
+    } else if (potatoes < farmCost) {
+        document.getElementById("potatoFarm").style.backgroundColor = '#006600';
     }
 }
 
+//////////////////////////////////////////////////////////////////////////////////
 
 function increaseSpudCount() {
     spudSpitter += 1;
@@ -64,7 +85,23 @@ function increaseFarmerCount() {
     console.log(farmer);
 }
 
+function increaseFarmCount() {
+    farm += 1;
+    potatoes -= farmCost;
+    farmCost = Math.floor(farmCost ** 1.04);
+    PpS += 5;
 
+    if (potatoes < farmCost) {
+        document.getElementById("potatoFarm").style.backgroundColor = '#006600';
+    }
+
+    document.getElementById("varCount").innerHTML = "Potatoes: " + potatoes;
+    document.getElementById("farmCount").innerHTML = "Potato Farm: " + farm;
+    document.getElementById("farmText").innerHTML = "Potato Farm &nbsp;&nbsp; || &nbsp;&nbsp;  Cost: " + farmCost;
+    console.log(farm);
+}
+
+// Core Functions
 //////////////////////////////////////////////
 
 function increasePotatoes() {
